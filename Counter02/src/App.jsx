91726e4ef0db7825978fb0a1a2e7a7b8a9023631
deeksha-0511 <deeksha.jain1008@gmail.com>
2024-02-01 20:11:05 +0530

    props

---
+++ Counter02/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  let [value, setValue ]= useState(0)
+ const addValue = ()=> {
+    setValue (value + 1 );
+
+ }
+ const removeValue = ()=> {
+  if(value > 0)setValue(value - 1);
+ }
+  return ( 
+    <>
+      <h1>Value: {value}</h1>
+      <button onClick={addValue}>Add Value: </button>
+      <button onClick={removeValue}>Remove Value:</button>
+
+    </>
+  )
+}
+
+export default App
---
+++ props1.4/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Cart from './components/Cart'
+
+function App() {
+
+  return (
+    <>
+   
+    <Cart name="Deeksha" age="25"/>
+    <Cart/>
+      
+    </>
+  )
+}
+
+export default App
