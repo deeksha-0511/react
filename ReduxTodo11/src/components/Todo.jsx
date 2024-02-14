0ae67e29a24927fbf2da190c01287bd4eefbcdc8
deeksha-0511 <deeksha.jain1008@gmail.com>
2024-02-14 21:02:44 +0530

    redux

---
+++ ReduxTodo11/src/components/Todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todoSlice';
+
+function Todo() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Todo;
