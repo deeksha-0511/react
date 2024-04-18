c99dfa268b81f384c043083744f1321ab58fb379
deeksha-0511 <deeksha.jain1008@gmail.com>
2024-04-18 20:07:37 +0530

    input

---
+++ Blog12/src/components/index.js
@@ -0,0 +1,39 @@
+
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Select from "./Select";
+
+
+import PostForm from "./post-form/PostForm";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Input from "./Input";
+import AuthLayout from "./AuthLayout";
+
+import Login from "./Login";
+import Button from "./Button";
+import PostCard from "./PostCard";
+
+
+
+
+
+export {
+    Select,
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input
+}
